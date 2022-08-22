f18ef138a05f19858bc1ab1d80e3a52a73add4f3
Josiah Webb <0xjosiah@Josiahs-MBP.lan>
2022-08-22 17:24:52 -0600

    basic set-up for speed typing game

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </a>
+      <h1>Keys Clacker</h1>
+      <textarea />
+      <h4>Time Remaining: 00:30</h4>
+      <button>Start</button>
+      <h1>Word Count: 0</h1>
+    </div>
+  )
+}
+
+export default App
