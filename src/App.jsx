4e5548ae2a16da841733e0fbcd0d397cb748306d
Josiah Webb <0xjosiah@Josiahs-MBP.lan>
2022-08-22 19:47:54 -0600

    shows word count after button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import { useState } from 'react'
 
 function App() {
+  const [words, setWords] = useState('')
+  const [count, setCount] = useState(0)
+
+  const handleChange = event => {
+    const {value} = event.target
+    setWords(value)
+    setCount(words.split(' ').length)
+  }
+  
+  const handleSubmit = () => {
+    setCount(words.trim().split(' ').length)
+  }
 
   return (
     <div>
       <a href="https://vitejs.dev" target="_blank">
         <img src="/vite.svg" className="logo" alt="Vite logo" />
       </a>
-      <h1>Keys Clacker</h1>
-      <textarea />
-      <h4>Time Remaining: 00:30</h4>
-      <button>Start</button>
-      <h1>Word Count: 0</h1>
+      <h1>Clackin' Keys</h1>
+      <textarea 
+        name='words'
+        value={words}
+        onChange={handleChange}
+      />
+      <h4>Time Remaining: ???</h4>
+      <button onClick={handleSubmit}>Start</button>
+      <h1>Word Count: {count}</h1>
     </div>
   )
 }
